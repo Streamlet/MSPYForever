1ebf3f33c5981faf94b9a3ce47a733bf778fec63
Streamlet <kjjreg@outlook.com>
2010-08-19 16:10:02 +0000

    add xl::QIPtr and its unit test

---
+++ SourceCode/xl/Objects/xlQIPtr.h
@@ -0,0 +1,154 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   xlQIPtr.h
+//    Author:      Streamlet
+//    Create Time: 2009-03-22
+//    Description: Smart pointer
+//
+//    Version history:
+//        2009-03-22 Created by Streamlet.
+//        2009-03-27 Released first version.
+//
+//
+//
+//------------------------------------------------------------------------------
+
+#ifndef __XLQIPTR_H_E6B64B03_95F8_4C7D_BC72_C3CE917F3DEF_INCLUDED__
+#define __XLQIPTR_H_E6B64B03_95F8_4C7D_BC72_C3CE917F3DEF_INCLUDED__
+
+
+namespace xl
+{
+    template <typename T>
+    class QIPtr
+    {
+    public:
+        QIPtr();
+        QIPtr(T *pData);
+        QIPtr(const QIPtr<T> &that);
+        QIPtr<T> &operator=(const QIPtr<T> &that);
+        ~QIPtr();
+
+    public:
+        bool operator==(const QIPtr<T> &that) const;
+        bool operator!=(const QIPtr<T> &that) const;
+        bool operator==(const T *pThat) const;
+        bool operator!=(const T *pThat) const;
+
+    public:
+        T &operator*() const;
+        T *operator->() const;
+
+    private:
+        void AddRef();
+        void Release();
+
+    private:
+        T *m_pData;
+        size_t *m_pcRefs;
+    };
+
+    template <typename T>
+    inline QIPtr<T>::QIPtr() : m_pData(nullptr), m_pcRefs(nullptr)
+    {
+    }
+
+    template <typename T>
+    inline QIPtr<T>::QIPtr(T *pData) : m_pData(pData), m_pcRefs(nullptr)
+    {
+        AddRef();
+    }
+
+    template <typename T>
+    inline QIPtr<T>::QIPtr(const QIPtr<T> &that) : m_pData(nullptr), m_pcRefs(nullptr)
+    {
+        this->m_pData = that.m_pData;
+        this->m_pcRefs = that.m_pcRefs;
+        this->AddRef();
+    }
+
+    template <typename T>
+    inline QIPtr<T> &QIPtr<T>::operator=(const QIPtr<T> &that)
+    {
+        if (this != &that && this->m_pData != that.m_pData)
+        {
+            this->Release();
+
+            this->m_pData = that.m_pData;
+            this->m_pcRefs = that.m_pcRefs;
+            this->AddRef();    
+        }
+        
+        return *this;
+    }
+
+    template <typename T>
+    inline QIPtr<T>::~QIPtr()
+    {
+        Release();
+    }
+
+    template <typename T>
+    inline void QIPtr<T>::AddRef()
+    {
+        if (m_pcRefs == nullptr)
+        {
+            m_pcRefs = new size_t;
+            *m_pcRefs = 0;
+        }
+
+        ++*m_pcRefs;
+    }
+
+    template <typename T>
+    inline void QIPtr<T>::Release()
+    {
+        if (m_pcRefs != nullptr && --*m_pcRefs == 0)
+        {
+            delete m_pcRefs;
+            delete[] m_pData;
+        }
+    }
+
+    template <typename T>
+    inline bool QIPtr<T>::operator==(const QIPtr<T> &that) const
+    {
+        return this->m_pData == that.m_pData;
+    }
+
+    template <typename T>
+    inline bool QIPtr<T>::operator!=(const QIPtr<T> &that) const
+    {
+        return this->m_pData != that.m_pData;
+    }
+
+    template <typename T>
+    inline bool QIPtr<T>::operator==(const T *pThat) const
+    {
+        return this->m_pData == pThat;
+    }
+
+    template <typename T>
+    inline bool QIPtr<T>::operator!=(const T *pThat) const
+    {
+        return this->m_pData != pThat;
+    }
+
+    template <typename T>
+    inline T &QIPtr<T>::operator*() const
+    {
+        return *this->m_pData;
+    }
+
+    template <typename T>
+    inline T *QIPtr<T>::operator->() const
+    {
+        return this->m_pData;
+    }
+
+} // namespace xl
+
+
+#endif // #ifndef __XLQIPTR_H_E6B64B03_95F8_4C7D_BC72_C3CE917F3DEF_INCLUDED__
---
+++ UnitTest/xlQIPtrTest/Main.cpp
@@ -0,0 +1,110 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   Main.cpp
+//    Author:      Streamlet
+//    Create Time: 2010-08-19
+//    Description: 
+//
+//    Version history:
+//
+//
+//
+//------------------------------------------------------------------------------
+
+
+#define _CRTDBG_MAP_ALLOC 
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include "../UnitTestFrame.h"
+#include <xl/Objects/xlQIPtr.h>
+
+using namespace xl;
+
+TEST_CASE(ctor)
+{
+    QIPtr<int> a;
+    TEST_ASSERT(a == nullptr);
+
+    int *pInt = new int;
+    *pInt = 123;
+
+    QIPtr<int> b(pInt), c(b);
+    TEST_ASSERT(b == pInt);
+    TEST_ASSERT(c == pInt);
+    TEST_ASSERT(b == c);
+
+    QIPtr<int> d(c);
+    TEST_ASSERT(d == c);
+    
+    QIPtr<int> e;
+    TEST_ASSERT(e == nullptr);
+
+    e = d;
+    TEST_ASSERT(e == pInt);
+}
+
+TEST_CASE(operator_set_compare)
+{
+    QIPtr<int> a, b;
+    TEST_ASSERT(a == nullptr);
+    TEST_ASSERT(!(a != nullptr));
+    TEST_ASSERT(b == nullptr);
+    TEST_ASSERT(!(b != nullptr));
+
+    int *pInt = new int;
+    *pInt = 123;
+
+    a = pInt;
+    TEST_ASSERT(a == pInt);
+    TEST_ASSERT(!(a != pInt));
+    TEST_ASSERT(a != b);
+    TEST_ASSERT(!(a == b));
+
+    b = a;
+    TEST_ASSERT(a == b);
+    TEST_ASSERT(!(a != b));
+
+    a = nullptr;
+    TEST_ASSERT(a == nullptr);
+    TEST_ASSERT(!(a != nullptr));
+    TEST_ASSERT(a != b);
+    TEST_ASSERT(!(a == b));
+}
+
+TEST_CASE(operator_point_ref)
+{
+    int *pInt = new int;
+    *pInt = 123;
+
+    QIPtr<int> a(pInt);
+    TEST_ASSERT(*a == 123);
+
+    struct s
+    {
+        int x;
+        int y;
+    };
+
+    s *pS = new s;
+    pS->x = 1;
+    pS->y = 2;
+
+    QIPtr<s> b(pS);
+    TEST_ASSERT(pS->x == 1);
+    TEST_ASSERT(pS->y == 2);
+}
+
+int main()
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    new int;
+
+    QIPtr<int> a(new int[10]);
+
+    return 0;
+}
+
