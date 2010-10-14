b7c212034593c87f13d9db17294a8c4fd85018b1
Streamlet <kjjreg@outlook.com>
2010-10-14 13:31:01 +0000

    add xlDef.cpp

---
+++ SourceCode/xl/xlDef.h
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   xlDef.h
+//    Author:      Streamlet
+//    Create Time: 2010-10-14
+//    Description: 
+//
+//    Version history:
+//
+//
+//
+//------------------------------------------------------------------------------
+
+#ifndef __XLDEF_H_1C304B99_7B3A_4BDB_9BE7_0C76AD3C1126_INCLUDED__
+#define __XLDEF_H_1C304B99_7B3A_4BDB_9BE7_0C76AD3C1126_INCLUDED__
+
+
+namespace xl
+{
+#if defined(_MSC_VER)
+#  define XL_SIZE_T_DEFINED
+#  if _MSC_VER >= 1600
+#    define XL_NULLPTR_DEFINED
+#  endif
+#elif defined(__GNUC__)
+#endif
+
+#ifndef XL_SIZE_T_DEFINED
+    typedef unsigned int size_t;
+#define _SIZE_T_DEFINED
+
+#ifndef XL_NULLPTR_DEFINED
+    const struct
+    {
+        template<typename T>
+        operator T * () const
+        {
+            return (T *)0;
+        }
+    } nullptr;  
+#endif
+
+} // namespace xl
+
+#endif // #ifndef __XLDEF_H_1C304B99_7B3A_4BDB_9BE7_0C76AD3C1126_INCLUDED__
