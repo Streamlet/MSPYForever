e16efbbf712b58b322217032821f07d0f4b3efd8
Streamlet <kjjreg@outlook.com>
2011-01-14 14:35:42 +0000

    add unit test project for xlTypeList

---
+++ UnitTest/xlTypeListTest/Main.cpp
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   Main.cpp
+//    Author:      Streamlet
+//    Create Time: 2011-01-14
+//    Description: 
+//
+//    Version history:
+//
+//
+//
+//------------------------------------------------------------------------------
+
+
+#include "../UnitTestFrame.h"
+#include <xl/Meta/xlTypeList.h>
+
+using namespace xl;
+
+int main()
+{
+    return 0;
+}
