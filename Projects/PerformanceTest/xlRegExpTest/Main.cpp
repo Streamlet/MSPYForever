b105c126120e9b0f152509e5502a93371227556e
Streamlet <kjjreg@outlook.com>
2012-06-11 14:36:46 +0000

    add performance test for xl::RegExp

---
+++ Projects/PerformanceTest/xlRegExpTest/Main.cpp
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   Main.cpp
+//    Author:      Streamlet
+//    Create Time: 2012-06-11
+//    Description: 
+//
+//    Version history:
+//
+//
+//
+//------------------------------------------------------------------------------
+
+
+int main()
+{
+    return 0;
+}
