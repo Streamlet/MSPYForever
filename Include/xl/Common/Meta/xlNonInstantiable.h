844d8c4c1d1f54af32668a4666b73c5a031ad53b
streamlet <streamlet@outlook.com>
2015-11-13 23:27:43 +0800

    add

---
+++ Include/xl/Common/Meta/xlNonInstantiable.h
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   xlNonInstantiable.h
+//    Author:      Streamlet
+//    Create Time: 2015-11-13
+//    Description: 
+//
+//------------------------------------------------------------------------------
+
+#ifndef __XLNONINSTANTIABLE_H_9903C400_3C1B_42F0_BF4A_3BC1DA68EEBD_INCLUDED__
+#define __XLNONINSTANTIABLE_H_9903C400_3C1B_42F0_BF4A_3BC1DA68EEBD_INCLUDED__
+
+
+namespace xl
+{
+    class NonInstantiable
+    {
+    private:
+        NonInstantiable();
+    };
+
+} // namespace xl
+
+#endif // #ifndef __XLNONINSTANTIABLE_H_9903C400_3C1B_42F0_BF4A_3BC1DA68EEBD_INCLUDED__
