f2169b2b1e5d15b18f01dd9b22d447b37dd90206
streamlet <streamlet@outlook.com>
2016-03-06 00:16:08 +0800

    bug

--- Include/xl/AppHelper/xlLog.h
+++ Include/xl/AppHelper/xlLog.h
@@ -24,15 +24,15 @@
 #include <stdio.h>
 
 // Define following macros before include this file
-// #define XL_LOG_PREFIX L"[LogPrefix] "             // Log prefix
-// #define XL_LOG_TARGET_DEBUGGER                       // Output to debugger
-// #define XL_LOG_TARGET_CONSOLE                        // Output to console
-// #define XL_LOG_TARGET_FILE L"%Temp%\xlLog.log"    // Output to file, supporting environment variables
-// #define XL_LOG_CONTENT_SOURCEFILE                    // Output source file name
-// #define XL_LOG_CONTENT_FUNCTION                      // Output function name
-// #define XL_LOG_CONTENT_LINE                          // Output line number
-#ifndef XL_LOG_LEVEL                                    // Output level
-#define XL_LOG_LEVEL    0xffffffff                      // Output all for default
+// #define XL_LOG_PREFIX L"[LogPrefix] "           // Log prefix
+// #define XL_LOG_TARGET_DEBUGGER                  // Output to debugger
+// #define XL_LOG_TARGET_CONSOLE                   // Output to console
+// #define XL_LOG_TARGET_FILE L"%Temp%\xlLog.log"  // Output to file, supporting environment variables
+// #define XL_LOG_CONTENT_SOURCEFILE               // Output source file name
+// #define XL_LOG_CONTENT_FUNCTION                 // Output function name
+// #define XL_LOG_CONTENT_LINE                     // Output line number
+#ifndef XL_LOG_LEVEL                            // Output level
+#define XL_LOG_LEVEL    0xffffffff              // Output all for default
 #endif
 
 namespace xl
@@ -79,16 +79,17 @@ namespace xl
 #ifdef XL_LOG_TARGET_FILE
             inline bool PrintToFile(const xl::String &strMessage)
             {
-                static CriticalSection csLogFile;
+                static Windows::CriticalSection csLogFile;
                 csLogFile.Lock();
 
                 wchar_t szPath[MAX_PATH] = {};
+
                 if (ExpandEnvironmentStrings(XL_LOG_TARGET_FILE, szPath, _countof(szPath)) == 0)
                 {
                     return false;
                 }
 
-                Handle hFile = CreateFile(szPath, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+                HANDLE hFile = CreateFile(szPath, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
 
                 if (hFile == INVALID_HANDLE_VALUE)
                 {
@@ -103,6 +104,8 @@ namespace xl
                     DWORD dwWritten = 0;
                     WriteFile(hFile, &wUnicodeBOM, sizeof(wUnicodeBOM), &dwWritten, NULL);
                 }
+
+                XL_ON_BLOCK_EXIT(CloseHandle, hFile);
 
                 LARGE_INTEGER li;
                 li.QuadPart = 0;
@@ -120,6 +123,12 @@ namespace xl
             inline void PrintMessage(LogLevel eLevel, const xl::String &strMessage, const xl::String &strSourceFile, const xl::String &strFunctionName, const xl::String &strLine)
             {
                 xl::String strOutput;
+
+                SYSTEMTIME st = {};
+                GetLocalTime(&st);
+                WCHAR szTime[] = L"0000-00-00 00:00:00.000 BUFFER";
+                swprintf_s(szTime, L"%04u-%02u-%02u %02u:%02u:%02u.%03u ", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+                strOutput += szTime;
 
 #ifdef XL_LOG_PREFIX
                 strOutput += XL_LOG_PREFIX;
@@ -232,9 +241,9 @@ namespace xl
                 PrintMessage(eLevel, szMessage, strSourceFile, strFunctionName, strLine);
             }
 
-#define XL_LOG_FILE        _T(__FILE__)
-#define XL_LOG_FUNC        _T(__FUNCTION__)
-#define XL_LOG_LINE        _T(XL_TOSTR(__LINE__))
+#define XL_LOG_FILE        XL_CONN(L, __FILE__)
+#define XL_LOG_FUNC        XL_CONN(L, __FUNCTION__)
+#define XL_LOG_LINE        XL_CONN(L, XL_TOSTR(__LINE__))
 
 #define XL_LOG_FUNCTION(level)      ::xl::AppHelper::Log::LogEnterFunction(level, XL_LOG_FILE, XL_LOG_FUNC, XL_LOG_LINE); \
                                     XL_ON_BLOCK_EXIT(::xl::AppHelper::Log::LogLeaveFunction, level, XL_LOG_FILE, XL_LOG_FUNC, XL_LOG_LINE)
